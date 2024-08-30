9356b8c3888e77fea25f5bfd88c8e74177a5530b
Si Gang Chen <sigang.chen@agfa.com>
2024-08-30 13:26:53 +0800

    cool18

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,38 @@
-// Copyright (c) 2024 Agfa HealthCare Corporation
-
+import { CheerioAPI } from 'cheerio';
 import Crawler from 'crawler';
+import fs from 'fs';
 
 const c = new Crawler({
   maxConnections: 10,
+  proxy: 'http://127.0.0.1:7890',
   // This will be called for each crawled page
   callback: (error, res, done) => {
     if (error) {
       console.log(error);
     } else {
-      const $ = res.$;
+      const $ = res.$ as CheerioAPI;
       // $ is Cheerio by default
       //a lean implementation of core jQuery designed specifically for the server
-      console.log($('#repository-link').text());
+      // fs.writeFileSync('test.html', $.html());
+      const $content = $('.show_content pre');
+      $content.find('font[color=#E6E6DD]').remove();
+      // $content.find('p').replaceWith('\n\n');
+      $content.find('p').each((i, el) => {
+        const pText = $(el).text();
+        $(el).replaceWith(`\n${pText}\n`);
+      });
+
+      $content.find('br').replaceWith('\n');
+      fs.writeFileSync('test.txt', $content.text());
     }
+
+    console.log('Crawled done');
     (done as any)();
   },
 });
 
 // Add just one URL to queue, with default callback
-c.add('https://www.npmjs.com/package/crawler');
+c.add('https://www.cool18.com/bbs4/index.php?app=forum&act=threadview&tid=14280908');
 /*
 // Add a list of URLs
 c.add(['http://www.google.com/', 'http://www.yahoo.com']);
